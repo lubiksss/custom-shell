800ce88f868df4f440c2a9df62b27cb28e2d0ae9
lubiksss <lubiksss@gmail.com>
2024-10-17 17:41:05 +0900

    test:

--- .zshrc
+++ .zshrc
@@ -176,5 +176,3 @@ setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording en
 #setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
 #setopt HIST_BEEP                 # Beep when accessing nonexistent history.
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- zsh-setting.sh
+++ zsh-setting.sh
@@ -16,6 +16,8 @@ cp -rf .p10k.zsh ~/.p10k.zsh
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >>~/.zshrc
 
 # syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
