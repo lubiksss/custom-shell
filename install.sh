d371bf77c424194d59dc599022bd3adcf3b2f3e3
lubiksss <lubiksss@gmail.com>
2024-10-17 16:08:37 +0900

    fix: Dont ask when install tzdata

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ $PREFIX apt-get install -y zsh
 $PREFIX apt-get install -y tmux
 $PREFIX apt-get install -y ripgrep
 
-export DEBIAN_FRONTED=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 $PREFIX apt-get install -y tzdata
 
 $PREFIX chsh -s `which zsh`
